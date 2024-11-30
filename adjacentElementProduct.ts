6f4eebb68d4254c9383db770354c7302a59e8ab5
AhmedHossam777 <ahmedhossam9226@gmail.com>
2024-11-30 04:19:57 +0200

    solve adjecent element prooduct

--- adjacentElementProduct.ts
+++ adjacentElementProduct.ts
@@ -1,5 +1,13 @@
 // for array input find the pair of adjacent element that give the largest product then return that product
 
 const adjacentElementProduct = (arr: number[]): number => {
-	return 0;
+	let max = arr[0] * arr[1];
+	for (let i = 0; i <= arr.length; i++) {
+		if (arr[i] * arr[i + 1] > max) {
+			max = arr[i] * arr[i + 1];
+		}
+	}
+	return max;
 };
+
+console.log(adjacentElementProduct([3, 6, -2, -5, 7, 3]));
